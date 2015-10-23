c4dd100dd5c4dfe465b676b969c484c09cffcf38
Marcin Ślusarz <marcin.slusarz@intel.com>
2015-10-23 17:31:26 +0200

    common: prefaulting
    Enabled by PMEM_PREALLOC_SPACE_CREATE (for new pools) and
    PMEM_PREALLOC_SPACE_OPEN (for pools not created with
    PMEM_PREALLOC_SPACE_CREATE) environment variables.

--- src/common/set.c
+++ src/common/set.c
@@ -1083,6 +1083,19 @@ util_header_check(struct pool_set *set, unsigned repidx, unsigned partidx,
 }
 
 /*
+ * util_replica_force_page_allocation - (internal) forces page allocation for
+ * replica
+ */
+static void
+util_replica_force_page_allocation(struct pool_replica *rep)
+{
+	volatile char *cur_addr = rep->part[0].addr;
+	char *addr_end = (char *)cur_addr + rep->part[0].size;
+	for (; cur_addr < addr_end; cur_addr += Pagesize)
+		*cur_addr = *cur_addr;
+}
+
+/*
  * util_header_check_remote -- (internal) validate header of a remote
  *                             pool set file
  */
@@ -1264,6 +1277,9 @@ util_replica_create(struct pool_set *set, unsigned repidx, int flags,
 	}
 
 	rep->is_pmem = pmem_is_pmem(rep->part[0].addr, rep->part[0].size);
+
+	if (getenv("PMEM_PREALLOC_SPACE_CREATE"))
+		util_replica_force_page_allocation(rep);
 
 	ASSERTeq(mapsize, rep->repsize);
 
@@ -1549,6 +1565,9 @@ util_replica_open(struct pool_set *set, unsigned repidx, int flags)
 	}
 
 	rep->is_pmem = pmem_is_pmem(rep->part[0].addr, rep->part[0].size);
+
+	if (getenv("PMEM_PREALLOC_SPACE_OPEN"))
+		util_replica_force_page_allocation(rep);
 
 	ASSERTeq(mapsize, rep->repsize);
 
